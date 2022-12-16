ea9becda1ab90462f50e625b9891cdd5f1039676
vietis-trinhbac <bactv@vietis.com.vn>
2022-12-16 17:03:38 +0700

    create login page

---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { capitalize } from '~/utils/str'
+
+// composable
+const { t } = useLang()
+
+// compiler macro
+definePageMeta({
+  layout: 'page',
+})
+useHead(() => ({
+  title: capitalize(t('pages.blank.title')),
+  meta: [
+    {
+      name: 'description',
+      content: t('pages.blank.description'),
+    },
+  ],
+}))
+</script>
+
+<template>
+  <PageWrapper>
+    <PageHeader>
+      <PageTitle :text="$t('pages.login.title')" class="capitalize" />
+    </PageHeader>
+    <PageBody>
+        <PageLoginForm>
+
+        </PageLoginForm>
+    </PageBody>
+  </PageWrapper>
+</template>
